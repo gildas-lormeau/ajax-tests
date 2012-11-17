994aa937409febb79cd84ff270efcc3a9e77b20f
ylesaout <ylesaout@gmail.com>
2012-11-17 17:36:12 +0100

    severs pure-html and js...

--- servers/hash-bang.js
+++ servers/hash-bang.js
@@ -1,6 +1,14 @@
 var express = require("express");
-
+var staticDirectory = __dirname + "../pure-js";
 var app = express();
-app.use(express["static"](__dirname + "../pure-html"));
 
+app.use(express["static"](staticDirectory));
+
+app.get("/discography", function(req, res, next) {
+    res.sendfile(staticDirectory + "/index.html");
+});
+
+app.get("/biography", function(req, res, next) {
+    res.sendfile(staticDirectory + "/index.html");
+});
 module.exports = app;
