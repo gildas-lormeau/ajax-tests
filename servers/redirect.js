0ec3c7c9fd868b24442040a1d30537fb45f2332b
Gildas Lormeau <gildas.lormeau@gmail.com>
2012-12-03 00:04:16 +0100

    fixed issue with redirection

--- servers/redirect.js
+++ servers/redirect.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 
-app.get("*", function (req, res) {
-
-    var url = "http://" + req.subdomains[req.subdomains.length - 1] + ".buzzmyfanclub.com" + req.originalUrl;
-    res.redirect(301, url);
+app.get("*", function(req, res) {
+	var subdomain = req.subdomains[req.subdomains.length - 1];
+	var url = "http://" + (subdomain == "fanclub" ? "buzzmyfanclub.com" : subdomain + "." + "buzzmyfanclub.com") + req.originalUrl;
+	res.redirect(301, url);
 });
 
 module.exports = app;
